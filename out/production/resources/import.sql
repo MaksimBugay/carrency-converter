42e7fb39cdc2a8e562657b98307082ee23792278
mbugai <maksim.bugai@onoffapp.com>
2018-01-15 17:17:01 +0200

    put out to ignore list

--- out/production/resources/import.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `role` VALUES (1,'ADMIN');
-INSERT INTO `role` VALUES (2,'USER');
-
-insert into user (active, city, country, birth_date, email, name, password, street, zip_code) values (1, 'Tallinn', 'Estonia', '29-11-1978', 'bmv2mail@gmail.com', 'Maksim', '$2a$10$9UDBermDWdZ.vZO3.Yi5c.KEdKyyuoOqjBzlWbLb3xiM3T1z0K.kG', 'Läänemere', '13914');
-
-insert into user_role(user_id, role_id) values(1, 2);
--- out/test/resources/login.feature
+++
@@ -1,6 +0,0 @@
-Feature: the success login
-
-  Scenario: login with valid credentials
-    Given user on the login page
-    When user logged in using username as "bmv2mail@gmail.com" and password as "dexter"
-    Then user should be on the home page
